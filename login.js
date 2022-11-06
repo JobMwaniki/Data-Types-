0d403424a4e50f8c06e9a21d31ea5fb791356bc9
JMnjuguna <jobmwanikinjuguna@gmail.com>
2022-11-06 14:54:34 +0300

    identation

--- login.js
+++ login.js
@@ -1,44 +1,42 @@
 function loginFunction() {
-    // LOGIC
+  // LOGIC
 
-    // Get the elements
-    const pageElements = getElements();
-    // get the Value of the elements
-    const pageValues = getValuesFromElements(pageElements);
-    // check whether the email contains @
-    emailChecker(pageValues.emailValue)
-    // check whether the password is more than 3 characters
-    passwordLengthChecker(pageValues.passwordValue);
-    //Log in successful
-
-};
+  // Get the elements
+  const pageElements = getElements();
+  // get the Value of the elements
+  const pageValues = getValuesFromElements(pageElements);
+  // check whether the email contains @
+  emailChecker(pageValues.emailValue);
+  // check whether the password is more than 3 characters
+  passwordLengthChecker(pageValues.passwordValue);
+  //Log in successful
+}
 
 function getElements() {
-    const emailElement = document.getElementById('email');
-    const passwordElement =document.getElementByID('password');
-    return { emailElement, passwordElement };
+  const emailElement = document.getElementById("email");
+  const passwordElement = document.getElementByID("password");
+  return { emailElement, passwordElement };
 }
 
 function getValuesFromElements(input) {
-    const emailElement = input.emailElement;
-    const passwordElement = input.passwordElement;
-    
-    const emailValue = emailElement.value;
-    const passwordValue = passwordElement.Value;
+  const emailElement = input.emailElement;
+  const passwordElement = input.passwordElement;
 
-    return {emailValue, passwordValue }
+  const emailValue = emailElement.value;
+  const passwordValue = passwordElement.Value;
 
+  return { emailValue, passwordValue };
 }
 
 function emailChecker(XXXX) {
-    if (XXXX.includes('@')) {
-        console.log('Valid Email');
-    }else {
-        alert('Hey, please check your email credentials and try again.');
-    }
+  if (XXXX.includes("@")) {
+    console.log("Valid Email");
+  } else {
+    alert("Hey, please check your email credentials and try again.");
+  }
 }
 function passwordLengthChecker(input) {
-    if (input.length < 3) {
-        alert('hey, please enter a password with 3 characters or more');
-    }
+  if (input.length < 3) {
+    alert("hey, please enter a password with 3 characters or more");
+  }
 }
